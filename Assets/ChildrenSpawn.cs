2d05faa87bcfd9d10562423c21d95517d1588fdb
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-01-21 14:06:53 -0800

    added some drowning children

--- Assets/ChildrenSpawn.cs
+++ Assets/ChildrenSpawn.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class ChildrenSpawn : MonoBehaviour {
 
 	public GameObject childPrefab;
+	public int xMin,xMax,yMin,yMax;
+
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+		InvokeRepeating (
+			"spawnChild", 1f,2f
+		);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void spawnChild(){
+		Instantiate(childPrefab, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin,yMax), 0), Quaternion.identity);
 	}
 }
