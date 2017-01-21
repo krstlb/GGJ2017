8d2fde587eed2c6311205fbbd9f0ee3e5922bc2d
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-01-21 14:49:02 -0800

    Magnet Waves and whitecaps not movin yet

---
+++ Assets/WhiteCaps.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteCaps : MonoBehaviour {
+
+	public int xMax,xMin, yMax,yMin;
+	public GameObject whiteCapPrefab;
+	// Use this for initialization
+	void Start () {
+		spawnWhiteCaps ();
+	}
+	void spawnWhiteCaps(){
+		for(int x = xMax; x>xMin; x--){
+			for(int y = yMax; y>yMin; y--){
+				Instantiate(whiteCapPrefab, new Vector3(Random.Range(x, x+0.5f), Random.Range(y,y+0.5f), 0), Quaternion.identity);
+			}
+		}
+	}
+}
