b2a5b0f1d80e2cd4fac3157442c28cfcd08bf8bd
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-01-22 13:28:53 -0800

    Added script to play crashing waves sound at random

---
+++ Assets/PlayAudioRandomly.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioRandomly : MonoBehaviour {
+	AudioSource audioSource;
+	private float randomTime = 10.0f;
+	private float timeCounter = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeCounter > randomTime) {
+			randomTime = Random.Range (8.0f, 12.0f);
+			timeCounter = 0.0f;
+			audioSource.Stop ();
+			audioSource.Play ();
+		} 
+
+		timeCounter += Time.deltaTime;
+	}
+}
