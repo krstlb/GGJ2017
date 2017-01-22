ff961195397cfde9c7ceea2ca3550c5509e56fdc
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-01-21 18:08:33 -0800

    Improve RGB values for wave colour changing

--- Assets/RandomColour.cs
+++ Assets/RandomColour.cs
@@ -8,9 +8,9 @@ public class RandomColour : MonoBehaviour {
 
 	void Start(){
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
-		rValue = Random.Range(0f, 0.2f);
-		gValue = Random.Range(0f, 0.6f);
-		bValue = Random.Range(0.2f, 1f);
+		rValue = Random.Range(0f, 0.3f);
+		gValue = Random.Range(0.3f, 0.9f);
+		bValue = Random.Range(0.9f, 1f);
 		changeColor(rValue, gValue, bValue);
 	}
 
