fb1ea9147dbb7cfbfd95617d53eefd5d77a27a30
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-01-25 17:07:53 -0800

    Add music to title screen and spawn splash sound todo: - Fix audio clip play trigger - Add instructional tooltip

--- Assets/Scripts/SpawnChildAndDie.cs
+++ Assets/Scripts/SpawnChildAndDie.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class SpawnChildAndDie : MonoBehaviour {
 	public GameObject child;
+	public AudioSource audioSource;
+	AudioClip audioClipSpawn;
 
 	// Use this for initialization
 	void Start () {
+		audioSource = GetComponent<AudioSource> ();
 		StartCoroutine (SpawnAndDie ());
 	}
 	
@@ -18,6 +21,10 @@ public class SpawnChildAndDie : MonoBehaviour {
 	IEnumerator SpawnAndDie() {
 		yield return new WaitForSeconds(2f);
 		Instantiate (child, this.gameObject.transform.position, Quaternion.identity);
+		audioClipSpawn = (AudioClip)Resources.Load ("Sound/spawnSplash");
+		audioSource.clip = audioClipSpawn;
+		audioSource.Stop ();
+		audioSource.Play ();
 		Destroy(gameObject);
 	}
 }
